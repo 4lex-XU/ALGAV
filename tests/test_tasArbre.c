1c3d4152db9c5e1a55ec56a8e1b51d4d5d70a53c
HUANG-Stephanie <115583805+HUANG-Stephanie@users.noreply.github.com>
2023-11-25 18:53:07 +0100

    Add files via upload

---
+++ tests/test_tasArbre.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../headers/clef128.h"
+#include "../headers/tasArbre.h"
+#include <time.h>
+#include <math.h>
+
+#define MAX 1000
+#define NB_CLEF 1000
+
+int main(int argc, char** argv){
+
+    if(argc < 2)
+    {
+        printf("Error: file missing\n");
+    }
+
+    FILE* file = NULL;
+    FILE* file2 = NULL;
+    
+    // OUVERTURE
+    if((file = fopen(argv[1], "r")) == NULL)
+    {
+        printf("Error: not open\n");
+    }
+
+    if((file2 = fopen(argv[2], "r")) == NULL)
+    {
+        printf("Error: not open\n");
+    }
+
+    Clef128* clefs[NB_CLEF];
+    Clef128* clefs2[NB_CLEF];
+    char buffer[MAX];
+    int i = 0, j = 0;
+
+    // LECTURE DES CLES DANS LE FICHIER 1
+    while(fgets(buffer, MAX, file) != NULL)
+    {
+        Clef128* clef = hexaToUnsigned(buffer);
+        clefs[i] = clef;
+        i++;
+    }
+
+    // LECTURE DES CLES DANS LE FICHIER 2
+    while(fgets(buffer, MAX, file2) != NULL)
+    {
+        Clef128* clef = hexaToUnsigned(buffer);
+        clefs2[j] = clef;
+        j++;
+    }
+          
+    // AJOUTS ITERATIFS
+    TasArbre* t1 = ajoutsIteratifs(clefs, 10, 1);
+    printf("-------AJOUTS ITERATIFS--------\n");
+    affichageTasArbre(t1);
+
+    /*
+    // SUPPRESSION
+    Clef128 min = supprMin(t1, t1);
+    printf("------CLEF MINIMUM------\n");
+    affichageClef(&min);
+    printf("-------SUPPRESSION--------\n");
+    affichageTasArbre(t1);
+    */
+
+    // CONSTRUCTION
+    TasArbre* t2 = construction(clefs, 10);
+    printf("\n");
+    printf("-------CONSTRUCTION--------\n");
+    affichageTasArbre(t2);
+    
+
+    /*
+    // UNION
+    TasArbre* tas1 = construction(clefs, 2);
+    TasArbre* tas2 = construction(clefs2, 2);
+    TasArbre* tasUnion = initialisation();
+    Union(tasUnion, tas1, tas2);
+    printf("-------UNION--------\n");
+    affichageTasArbre(tasUnion);
+    */
+
+
+
+    return 0;
+}
