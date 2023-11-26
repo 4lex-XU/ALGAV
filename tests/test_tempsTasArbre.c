40f78153f5b350a0c7b19ea645407a9596fcb559
HUANG-Stephanie <h810.stephanie@gmail.com>
2023-11-26 13:44:52 +0100

    commit

---
+++ tests/test_tempsTasArbre.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../headers/clef128.h"
+#include "../headers/tasTableau.h"
+#include <time.h>
+#include <math.h>
+
+#define MAX 1000
+
