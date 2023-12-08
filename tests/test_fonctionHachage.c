09712eff9682c92e932202ccf618b09a0a0679f4
HUANG-Stephanie <h810.stephanie@gmail.com>
2023-12-08 23:27:35 +0100

    commit_hash

--- tests/test_fonctionHachage.c
+++ tests/test_fonctionHachage.c
@@ -8,9 +8,12 @@
 int main(){
 
     unsigned int* md5 = MD5("Et l’unique cordeau des trompettes marines");
-    printf("MD5 = %02x %02x %02x %02x\n", md5[0], md5[1], md5[2], md5[3]);
-
+    printf("MD5 = %08x %08x %08x %08x\n", md5[0], md5[1], md5[2], md5[3]);
     free(md5);
+
+    unsigned int* md5bis = MD5("Et l’unique cordeau des trompettes marinEs");
+    printf("MD5 = %08x %08x %08x %08x\n", md5[0], md5[1], md5[2], md5[3]);
+    free(md5bis);
     
     return 0;
 }
